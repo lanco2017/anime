c2da83e117afd728641adb6fd5f60617c1d03ef3
synr <synr@users.noreply.github.com>
2016-11-20 02:41:47 +0800

    Update main.go

--- main.go
+++ main.go
@@ -89,7 +89,7 @@ func anime(text string) string {
 				print_string = anime_say + "http://ani.gamer.com.tw/animeVideo.php?sn=6298" + "\n\n等等！這是最後一話！？"
 			default:
 			}
-		case "歌之☆王子殿下♪ 真愛","歌王子","uta","":
+		case "歌之☆王子殿下♪ 真愛 LEGEND STAR","歌王子","uta","":
 			//reg.ReplaceAllString(text, "$2")
 			switch reg.ReplaceAllString(text, "$3") {
 			case "1":
@@ -119,10 +119,10 @@ func anime(text string) string {
 			default:
 			}
 		default:
-			print_string = "對不起，我找不到這部動畫，我還沒學呢...\n我目前知道的動畫還很少，因為我考試不及格QAQ\n\n（其實是因為開發者純手動輸入，沒用資料庫跟爬蟲。才會增加比較慢XD）"
+			print_string = "對不起，我找不到這部動畫，我還沒學呢...\n我目前知道的動畫還很少，因為我考試不及格QAQ"
 		}
 	default:
-		print_string = "HI～ 我最近很喜歡看巴哈姆特動畫瘋。\nhttp://ani.gamer.com.tw/\n\n你也可以問我動畫，我可以帶你去看！\n要問我動畫的話可以這樣問：\n動畫 動畫名稱 集數\n\n例如：\n動畫 美術社 12\nアニメ 美術社大有問題 12\nanime 美術社 １\n巴哈姆特 美術社 12\n以上這些都可以\n\n但中間要用半形空白隔開喔！\n不然我會看不懂＞A＜\n\nPS：目前這隻喵只提供查詢動畫的功能"
+		print_string = "HI~ 我最近很喜歡看巴哈姆特動畫瘋。\nhttp://ani.gamer.com.tw/\n\n你也可以問我動畫，我可以帶你去看！\n要問我動畫的話可以這樣問：\n動畫 動畫名稱 集數\n\n例如：\n動畫 美術社 12\nアニメ 美術社大有問題 12\nanime 美術社 １\n巴哈姆特 美術社 12\n以上這些都可以\n\nＰＳ：目前功能還很少XD"
 	}
 	return print_string
 }
@@ -178,7 +178,3 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 }
-
-
-
-
