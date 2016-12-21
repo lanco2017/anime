f077bc6d897e0602b891135ee73e3229fb326740
synr <oooo00owo00oooo@gmail.com>
2016-12-22 02:30:07 +0800

    只能四個按鈕

--- main.go
+++ main.go
@@ -1068,9 +1068,10 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 						//https://github.com/line/line-bot-sdk-go
 						//https://github.com/line/line-bot-sdk-go/blob/master/linebot/message.go
 
-						//模板成功  //官方範例 https://github.com/line/line-bot-sdk-go/blob/master/examples/kitchensink/server.go
-						// //1
-						// //.NewConfirmTemplate 模板
+					//模板成功  //官方範例 https://github.com/line/line-bot-sdk-go/blob/master/examples/kitchensink/server.go
+						//linebot.NewTemplateMessage
+						// //1 confirm
+						// //.NewConfirmTemplate 模板，裡面最多只能有兩個動作，按鈕只能左右
 						// //.NewMessageTemplateAction 發言動作
  					//     leftBtn := linebot.NewMessageTemplateAction("left", "left clicked")// 後面的參數 "left clicked" = 在使用者按下後，自動幫使用者發訊息
  					//     rightBtn := linebot.NewMessageTemplateAction("right", "right clicked")// 後面的參數 "right clicked" = 在使用者按下後，自動幫使用者發訊息
@@ -1078,21 +1079,23 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
  					//     template := linebot.NewConfirmTemplate("Hello World", leftBtn, rightBtn)
  					//     //.NewConfirmTemplate
 
- 					    //2
- 					    //.NewButtonsTemplate 模板
+						//linebot.NewTemplateMessage
+ 					    //2 buttons
+ 					    //.NewButtonsTemplate 模板，裡面最多只能有四個動作
  					    //.NewURITemplateAction 開啟指定網址的動作
  					    //.NewPostbackTemplateAction ？？動作
  					    //						第二參數可以讓她　ＰＯＳＴ指定內容（但還不會處理．．．）	第三參數類似於 .NewMessageTemplateAction 的效果
- 					    imageURL := "https://images.gamme.com.tw/news2/2016/51/39/paCYoqCXkqSarqSZ.jpg"
-						template := linebot.NewButtonsTemplate(
-							imageURL, "你好歡迎光臨", "這是內文",							//這前三個 分別是圖片(必須https)、標題、內文
-							linebot.NewURITemplateAction("來我的網站", "https://synr.github.io"),
-							linebot.NewPostbackTemplateAction("目錄查詢", "目錄", "目錄"),
-							linebot.NewPostbackTemplateAction("開發者", "開發者", "開發者"),
-							linebot.NewMessageTemplateAction("Say message", "Rice=米"),
-							linebot.NewMessageTemplateAction("Say message2", "Rice=米"),
-							linebot.NewMessageTemplateAction("Say message3", "Rice=米"),
-						)
+ 					//     imageURL := "https://images.gamme.com.tw/news2/2016/51/39/paCYoqCXkqSarqSZ.jpg"
+						// template := linebot.NewButtonsTemplate(
+						// 	imageURL, "你好歡迎光臨", "這是內文",							//這前三個 分別是圖片(必須https)、標題、內文
+						// 	linebot.NewURITemplateAction("來我的網站", "https://synr.github.io"),
+						// 	linebot.NewPostbackTemplateAction("目錄查詢", "目錄", "目錄"),
+						// 	linebot.NewPostbackTemplateAction("開發者", "開發者", "開發者"),
+						// 	linebot.NewMessageTemplateAction("Say message", "Rice=米"),
+						// )
+
+						//linebot.NewTemplateMessage
+						//3 carousel
 
  					    obj_message := linebot.NewTemplateMessage("HI～ 我最近很喜歡看巴哈姆特動畫瘋。\nhttp://ani.gamer.com.tw/\n\n你也可以問我動畫，我可以帶你去看！\n要問我動畫的話可以這樣問：\n動畫 動畫名稱 集數\n\n例如：\n動畫 美術社 12\nアニメ 美術社大有問題 12\nanime 美術社 １\n巴哈姆特 美術社 12\n以上這些都可以\n\n但中間要用空白或冒號、分號隔開喔！\n不然我會看不懂 ＞A＜\n\nPS：目前這隻喵只提供查詢動畫的功能。\n如有其他建議或想討論，請對這隻貓輸入「開發者」進行聯絡。", template)//messgage := linebot.NewTemplateMessage("請使用更新 APP 或使用手機 APP 才能看到這個功能。", template)
  					    //.NewTemplateMessage("無法支援按鈕模式時要發出的訊息",ConfirmTemplate 物件)
@@ -1107,15 +1110,16 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 
 						//++ https://github.com/dongri/line-bot-sdk-go KEY:linebot.NewImageMessage
 
-						//發圖片成功
+						//.NewImageMessage 發圖片成功
 						//originalContentURL := "https://avatars0.githubusercontent.com/u/5731891?v=3&s=96"
     					//previewImageURL := "https://avatars0.githubusercontent.com/u/5731891?v=3&s=96"
     					//obj_message := linebot.NewImageMessage(originalContentURL, previewImageURL)
 
-						//發貼貼圖成功						
+						//.NewStickerMessage 發貼貼圖成功	 //https://devdocs.line.me/files/sticker_list.pdf					
 						//obj_message := linebot.NewStickerMessage("1", "1") //https://devdocs.line.me/en/?go#send-message-object
+
+ 					    //接收各種 message object
 						 if _, err = bot.ReplyMessage(event.ReplyToken, obj_message).Do(); err != nil {
-						//https://devdocs.line.me/files/sticker_list.pdf
 						//if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewStickerMessage("1", "1"),linebot.NewStickerMessage("1", "2"),linebot.NewStickerMessage("2", "19"),linebot.NewStickerMessage("2", "20"),linebot.NewStickerMessage("1", "3")).Do(); err != nil {
 							log.Print(err)
 						}
