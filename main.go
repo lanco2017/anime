6e1d7ad37cc35d82f55b4d76eed1cf779f91104d
synr <oooo00owo00oooo@gmail.com>
2016-12-21 16:11:03 +0800

    撤掉測試

--- main.go
+++ main.go
@@ -1148,7 +1148,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 						// 				}
 									//https://devdocs.line.me/en/#webhook-event-object
 				log.Print("對方丟圖片 message.ID = " + message.ID)
-				log.Print("對方丟圖片 message.Text = " + message.Text)
+
 				//log.Print("對方丟圖片 linebot.EventSource = " + linebot.EventSource
 
 				//----------------------------------------------------------------取得使用者資訊的寫法
