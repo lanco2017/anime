8420d48a27fefac9d699ed3b18bab9c9587dd0a2
synr <oooo00owo00oooo@gmail.com>
2016-12-21 13:13:05 +0800

    message.type 拿掉

--- main.go
+++ main.go
@@ -1148,7 +1148,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 						// 					log.Print(err)
 						// 				}
 									//https://devdocs.line.me/en/#webhook-event-object
-				log.Print("對方丟圖片 message.type = " + message.type)
+				//log.Print("對方丟圖片 message.type = " + message.type)
 				log.Print("對方丟圖片 message.ID = " + message.ID)
 				log.Print("對方丟圖片 message.originalContentUrl = " + message.originalContentUrl)
 				log.Print("對方丟圖片 message.previewImageUrl = " + message.previewImageUrl)
