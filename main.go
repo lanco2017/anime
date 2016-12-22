9b7f8060283c78f1a7f77a0b71b2235136a533c9
synr <oooo00owo00oooo@gmail.com>
2016-12-22 08:03:21 +0800

    test

--- main.go
+++ main.go
@@ -1216,8 +1216,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 								linebot.NewCarouselColumn(
 									"https://p2.bahamut.com.tw/B/2KU/18/0001484818.PNG", "意見反饋 feedback", "你可以透過此功能跟開發者提出建議",
 									linebot.NewMessageTemplateAction("聯絡 LINE 機器人開發者", "開發者"),
-									inebot.NewURITemplateAction("線上與開發者聊天", "http://www.smartsuppchat.com/widget?key=77b943aeaffa11a51bb483a816f552c70e322417&vid=" + target_user +
-		"&lang=tw&pageTitle=%E9%80%99%E6%98%AF%E4%BE%86%E8%87%AA%20LINE%40%20%E9%80%B2%E4%BE%86%E7%9A%84%E5%8D%B3%E6%99%82%E9%80%9A%E8%A8%8A"),
+									inebot.NewURITemplateAction("線上與開發者聊天", "http://www.smartsuppchat.com/widget?key=77b943aeaffa11a51bb483a816f552c70e322417&vid=" + target_user +	"&lang=tw&pageTitle=LINEBOT"),
 									linebot.NewURITemplateAction("加開發者 LINE", "https://line.me/R/ti/p/@uwk0684z"),
 								),
 							)
