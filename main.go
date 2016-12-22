a121a82cee4d475f4db473afdcdbc7f4b3a9dc15
synr <oooo00owo00oooo@gmail.com>
2016-12-22 08:37:27 +0800

    LINE OK

--- main.go
+++ main.go
@@ -1221,12 +1221,6 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 									linebot.NewURITemplateAction("線上與開發者聊天", "http://www.smartsuppchat.com/widget?key=77b943aeaffa11a51bb483a816f552c70e322417&vid=" + target_user + "&lang=tw&pageTitle=%E9%80%99%E6%98%AF%E4%BE%86%E8%87%AA%20LINE%40%20%E9%80%B2%E4%BE%86%E7%9A%84%E5%8D%B3%E6%99%82%E9%80%9A%E8%A8%8A"),
 									linebot.NewMessageTemplateAction("聯絡 LINE 機器人開發者", "開發者"),
 								),
-								linebot.NewCarouselColumn(
-									"https://trello-attachments.s3.amazonaws.com/52ff05f27a3c676c046c37f9/5831e5e304f9fac88ac50a23/c2704b19816673a30c76cdccf67bcf8f/2016_-_%E8%A4%87%E8%A3%BD.png", "意見反饋 feedback", "你可以透過此功能\n跟開發者提出建議",
-									linebot.NewURITemplateAction("電話測試", "tel:+886921123177"),
-									linebot.NewURITemplateAction("線上與開發者聊天", "http://www.smartsuppchat.com/widget?key=77b943aeaffa11a51bb483a816f552c70e322417&vid=" + target_user + "&lang=tw&pageTitle=%E9%80%99%E6%98%AF%E4%BE%86%E8%87%AA%20LINE%40%20%E9%80%B2%E4%BE%86%E7%9A%84%E5%8D%B3%E6%99%82%E9%80%9A%E8%A8%8A"),
-									linebot.NewMessageTemplateAction("聯絡 LINE 機器人開發者", "開發者"),
-								),
 							)
 							obj_message := linebot.NewTemplateMessage(bot_msg, template)
 							if _, err = bot.ReplyMessage(event.ReplyToken,obj_message).Do(); err != nil {
