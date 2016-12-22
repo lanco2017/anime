b75aacf7f881a0c302fc0e6029373f4d2d4c4eb0
synr <oooo00owo00oooo@gmail.com>
2016-12-22 19:23:08 +0800

    ＰＯＳＴ　ｔｅｓｔ

--- main.go
+++ main.go
@@ -49,7 +49,7 @@ func HttpPost_JANDI(body, connectColor, title, url string) error {
 		"connectColor":"` + connectColor + `",
 		"connectInfo" : [{
 				"title" : "` + title + `",
-				"description" : "這是經由 LINE BOT 轉來的同步消息"
+				"description" : "這是經由 LINE BOT 轉來的同步消息",
 				"imageUrl": "https://line.me/R/ti/p/@pyv6283b"
 			}]
 		}`
