bbdabcca34fcd0252c5d3f90a2ca6d00c523075f
synr <oooo00owo00oooo@gmail.com>
2016-12-26 08:28:13 +0800

    de

--- main.go
+++ main.go
@@ -1639,7 +1639,6 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 													linebot.NewPostbackTemplateAction("請機器人離開群組","離開群組", "機器人已經自動離開。\n如要加回來請找 https://line.me/R/ti/p/@uwk0684z"),
 													linebot.NewURITemplateAction("加開發者 LINE", "https://line.me/R/ti/p/@uwk0684z"),
 													linebot.NewURITemplateAction("線上與開發者聊天", "http://www.smartsuppchat.com/widget?key=77b943aeaffa11a51bb483a816f552c70e322417&vid=" + target_id_code + "&lang=tw&pageTitle=%E9%80%99%E6%98%AF%E4%BE%86%E8%87%AA%20LINE%40%20%E9%80%B2%E4%BE%86%E7%9A%84%E5%8D%B3%E6%99%82%E9%80%9A%E8%A8%8A"),
-													linebot.NewPostbackTemplateAction("聯絡 LINE 機器人開發者", "開發者", "開發者"),
 												),
 											)
 											obj_message := linebot.NewTemplateMessage("這是命令機器人自己離開群組的方法。\n請用 APP 端查看下一步。", template)
