d40d7bb272838eddac3231329e24f3a907067e0f
lanco2017 <oooolan.luckyoooo@gmail.com>
2017-03-02 20:35:59 +0800

    ++

--- main.go
+++ main.go
@@ -1185,6 +1185,8 @@ func anime(text string,user_msgid string,reply_mode string) string {
 				print_string = anime_say + "http://ani.gamer.com.tw/animeVideo.php?sn=7345"
 			case "7","07":
 				print_string = anime_say + "http://ani.gamer.com.tw/animeVideo.php?sn=7346"
+			case "8","08":
+				print_string = anime_say + "http://ani.gamer.com.tw/animeVideo.php?sn=7347"
 			default:
 			}
 		case "終物語":
