00ed811a718667e59d079aa284ae70ceeeeb7f70
lanco2017 <oooolan.luckyoooo@gmail.com>
2017-03-19 15:49:21 +0800

    ++

--- main.go
+++ main.go
@@ -2965,6 +2965,8 @@ func anime(text string,user_msgid string,reply_mode string) string {
 				print_string = anime_say + "https://ani.gamer.com.tw/animeVideo.php?sn=7402"
 			case "21":
 				print_string = anime_say + "https://ani.gamer.com.tw/animeVideo.php?sn=7492"
+			case "22":
+				print_string = anime_say + "https://ani.gamer.com.tw/animeVideo.php?sn=7493"
 			default:
 				print_string = anime_say + "http://ani.gamer.com.tw/animeVideo.php?sn=6479"
 			}
